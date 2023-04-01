2ffe5aad93221d458e5af48ca4d66559baffe95a
Amzad100 <mdamzad2003a@gmail.com>
2023-04-01 23:31:32 +0600

    some change and all ok

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
         <>
             <nav className="navbar bg-body-tertiary">
                 <div className="container-fluid">
-                    <h1>Oeean of Education</h1>
+                    <h1>Ocean of Education</h1>
                     <div className="d-flex navbar-brand">
                     <img className="rounded-circle" width="50" height="50" src="https://scontent.fcgp3-2.fna.fbcdn.net/v/t39.30808-6/335924063_240219231690266_5908910972992035526_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=0debeb&_nc_eui2=AeEUunv1J9OH8_C-zi2YyfT_IRsIZSZCo3ghGwhlJkKjeKeoqp-vp88AsmBueDk5wplgmt-WOfXuf8UXEt9uJa3a&_nc_ohc=OEk7dL7QgtAAX8KRxxx&_nc_ht=scontent.fcgp3-2.fna&oh=00_AfAkY6YJiGs1QFWKrGQMnCGQ8gGq5OgseGpozZu-xxa3Pg&oe=642ADEC6" alt="Logo" />
                     </div>
